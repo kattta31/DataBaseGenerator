89cb52f5ce3060cbb422d4c2041dfc6a470a3f5e
ADANI\zabrodin <rediskal50@gmail.com>
2024-10-08 22:20:49 +0300

    ref: add first Test

---
+++ DataBaseGenerator.Test/AutoTestSuite.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataBaseGenerator.Test.Core;
+using DataBaseGenerator.Test.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataBaseGenerator.Test
+{
+    [TestClass]
+    [DoNotParallelize]
+    public class AutoTestSuite
+    {
+        private readonly NLog.ILogger _logger = NLog.LogManager.GetCurrentClassLogger();
+        private ITestClient _testClient;
+
+        private string _pathToTestClient = "D:\\Develop\\DataBaseGenerator\\DataBaseGenerator\\bin\\Debug\\net8.0-windows\\DataBaseGenerator.UI.Wpf.exe";
+
+        public AutoTestSuite()
+        {
+            _testClient = new DataBaseTestClient(_pathToTestClient);
+        }
+
+
+        [TestMethod]
+        public async Task TestStartTestClient()
+        {
+            try
+            {
+                _logger.Info("Entering in Test Start test client");
+
+                var menuState = await _testClient.StartAsync(TimeSpan.FromSeconds(30));
+                Assert.IsNotNull(menuState);
+
+            }
+            catch (Exception exception)
+            {
+                _logger.Error(exception, "Test Go To Patient Selecting State Failed");
+
+                throw;
+            }
+            finally
+            {
+                _testClient.Kill();
+            }
+        }
+
+
+    }
+}
