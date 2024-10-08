d2ca2ed14909b72b3a7280cc7fe9e8e4919448ce
ADANI\zabrodin <rediskal50@gmail.com>
2024-10-08 22:19:26 +0300

    ref: create locator

---
+++ DataBaseGenerator.Test/Locators/MainWindowStateLocators.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+
+namespace DataBaseGenerator.Test.Locators
+{
+    public class MainWindowStateLocators
+    {
+
+        static Window _window;
+        static ConditionFactory _conditionFactory;
+
+        public MainWindowStateLocators(Window window, ConditionFactory conditionFactory)
+        {
+            _window = window;
+            _conditionFactory = conditionFactory;
+        }
+
+        public AutomationElement ConnectButton => _window.FindFirstDescendant(_conditionFactory.ByName("ConnectButton")).AsButton();
+    }
+}
