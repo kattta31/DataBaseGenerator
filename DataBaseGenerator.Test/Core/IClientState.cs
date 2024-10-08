d9b1f84a892761221344f1dbbba5f15de4dc7d96
ADANI\zabrodin <rediskal50@gmail.com>
2024-10-08 22:18:19 +0300

    ref: add Core for AutoTest project

---
+++ DataBaseGenerator.Test/Core/IClientState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlaUI.Core.AutomationElements;
+
+namespace DataBaseGenerator.Test.Core
+{
+    public interface IClientState
+    {
+        string Name { get; }
+
+        Task<IClientState> GoToStateAsync(string stateName, TimeSpan timeout);
+
+        Window GetMainWindow();
+
+        IClientState ToNextState();
+
+        bool IsState(Window window);
+
+    }
+}
---
+++ DataBaseGenerator.Test/Core/ITestClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseGenerator.Test.Core
+{
+    public interface ITestClient
+    {
+        Task<IClientState> StartAsync(TimeSpan timeout);
+        void Kill();
+    }
+}
